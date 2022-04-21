1000161f99f63e7f997ca5ff99594c2bd53cfd25
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-04-21 15:43:04 +0200

    :art: Style code

--- hydra_app/__init__.py
+++ hydra_app/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.0'
-__author__ = 'Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>'
+__version__ = "0.1.0"
+__author__ = "Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>"
 __all__ = []
