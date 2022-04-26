e20d2c49325df4b12adc262c7e4bfc65f992ba93
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-04-26 11:17:02 +0200

    :sparkles: Allow user configuration

--- hydra_app/model/network.py
+++ hydra_app/model/network.py
@@ -1,11 +1,12 @@
 __all__ = ["create_model"]
 
 from dataclasses import dataclass
-from pathlib import Path
 
 import hydra
+from omegaconf import DictConfig, OmegaConf
 
 import hydra_app.conf
+
 
 @dataclass
 class NeuralNetwork:
@@ -17,10 +18,14 @@ class NeuralNetwork:
     lr: float
 
 
-def create_model() -> "NeuralNetwork":
+def create_model(user_config: DictConfig) -> "NeuralNetwork":
     """Factory method. Returns a neural network object created using default
-    configuration.
+    and user configuration.
     """
     with hydra.initialize_config_module(hydra_app.conf.__name__):
-        config = hydra.compose("main")
+        base_config = hydra.compose("main")
+        if user_config is not None:
+            config = OmegaConf.merge(base_config, user_config)
+        else:
+            config = base_config
         return hydra.utils.instantiate(config.model)
